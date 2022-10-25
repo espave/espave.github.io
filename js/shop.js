d2d4a51aa653bcde4f573e93a3b10d487e0288df
espaveclothing@gmail.com <espaveclothing@gmail.com>
2022-10-25 08:53:31 -0500

    Update Json database and shop parsing and Index menu and shop menu

--- js/shop.js
+++ js/shop.js
@@ -37,30 +37,61 @@ category = urlParams.get('category');
     let output = '';
     $.each(products, (_index, product) => {
 
-            if (product.PType != null) {
-                if (product.PType.toLowerCase().includes(producttype.toLowerCase()) &&
-                    (product.Category.toLowerCase().includes(category.toLowerCase()))) {
-                    output += `
-          <div class="col-lg-4 col-md-6 col-sm-12 pb-1">
-          <div class="card product-item border-0 mb-4">
-              <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-                  <img class="img-fluid w-100" src="${product.PicUrl}" alt="">
-              </div>
-              <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
-                  <h6 class="text-truncate mb-3">${product.Name}</h6>
-                  <div class="d-flex justify-content-center">
-                      <h6>$${product.Price}</h6>
-                  </div>
-              </div>
-              <div class="card-footer d-flex justify-content-between bg-light border">
-                  <a href="detail.html?productid=${product.PID}" class="btn btn-sm text-dark p-0"><i class="fas fa-eye text-primary mr-1"></i>Detalle</a>
-                  <a href="" class="btn btn-sm text-dark p-0"><i class="fas fa-shopping-cart text-primary mr-1"></i>Comprar</a>
-              </div>
-          </div>
-      </div>
-        `;
-                }
-            }
+      if(producttype == null)
+      {
+              output += `
+                        <div class="col-lg-4 col-md-6 col-sm-12 pb-1">
+                        <div class="card product-item border-0 mb-4">
+                            <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
+                                <img class="img-fluid w-100" src="${product.PicUrl}" alt="">
+                            </div>
+                            <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
+                                <h6 class="text-truncate mb-3">${product.Name}</h6>
+                                <div class="d-flex justify-content-center">
+                                    <h6>$${product.Price}</h6>
+                                </div>
+                            </div>
+                            <div class="card-footer d-flex justify-content-between bg-light border">
+                                <a href="detail.html?productid=${product.PID}" class="btn btn-sm text-dark p-0"><i class="fas fa-eye text-primary mr-1"></i>Detalle</a>
+                                <a href="" class="btn btn-sm text-dark p-0"><i class="fas fa-shopping-cart text-primary mr-1"></i>Comprar</a>
+                            </div>
+                        </div>
+                    </div>
+                      `;
+      }
+
+      else
+      {
+                if (product.PType != null) {
+                  if (product.PType.toLowerCase().includes(producttype.toLowerCase()) &&
+                      (product.Category.toLowerCase().includes(category.toLowerCase()))) {
+                      output += `
+            <div class="col-lg-4 col-md-6 col-sm-12 pb-1">
+            <div class="card product-item border-0 mb-4">
+                <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
+                    <img class="img-fluid w-100" src="${product.PicUrl}" alt="">
+                </div>
+                <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
+                    <h6 class="text-truncate mb-3">${product.Name}</h6>
+                    <div class="d-flex justify-content-center">
+                        <h6>$${product.Price}</h6>
+                    </div>
+                </div>
+                <div class="card-footer d-flex justify-content-between bg-light border">
+                    <a href="detail.html?productid=${product.PID}" class="btn btn-sm text-dark p-0"><i class="fas fa-eye text-primary mr-1"></i>Detalle</a>
+                    <a href="" class="btn btn-sm text-dark p-0"><i class="fas fa-shopping-cart text-primary mr-1"></i>Comprar</a>
+                </div>
+            </div>
+        </div>
+          `;
+                  }
+              }
+              
+      }
+
+
+
+
         });
   
     $('#productlist').html(output);
