22a9cdc4007aa75e691a253d16503e3992010966
espaveclothing@gmail.com <espaveclothing@gmail.com>
2022-11-06 14:12:15 -0500

    adjusted detailed page images carusel to 3

--- js/detail.js
+++ js/detail.js
@@ -29,16 +29,13 @@ console.log(productid);
                 <div id="product-carousel" class="carousel slide" data-ride="carousel">
                     <div class="carousel-inner border">
                         <div class="carousel-item active">
+                            <img class="w-100 h-100" src="img/${product.PID}.jpg" alt="Image">
+                        </div>
+                        <div class="carousel-item">
                             <img class="w-100 h-100" src="img/${product.PID}-1.jpg" alt="Image">
                         </div>
                         <div class="carousel-item">
                             <img class="w-100 h-100" src="img/${product.PID}-2.jpg" alt="Image">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="w-100 h-100" src="img/${product.PID}-3.jpg" alt="Image">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="w-100 h-100" src="img/${product.PID}-4.jpg" alt="Image">
                         </div>
                     </div>
                     <a class="carousel-control-prev" href="#product-carousel" data-slide="prev">
