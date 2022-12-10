9d8af2ec6d5102e579427adfb3ef3ba08cec5a64
Katherine Avila <espaveclothing@gmail.com>
2022-12-09 19:14:03 -0500

    hola

--- js/detail.js
+++ js/detail.js
@@ -22,55 +22,109 @@ console.log(productid);
 
                 if (product.PID != null) {
                   if (product.PID.toLowerCase().includes(productid.toLowerCase())) {
-                      output += `
-        <div class="container-fluid py-5">
-        <div class="row px-xl-5">
-            <div class="col-lg-5 pb-5">
-                <div id="product-carousel" class="carousel slide" data-ride="carousel">
-                    <div class="carousel-inner border">
-                        <div class="carousel-item active">
-                            <img class="w-100 h-100" src="img/${product.PID}.jpg" alt="Image">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="w-100 h-100" src="img/${product.PID}-1.jpg" alt="Image">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="w-100 h-100" src="img/${product.PID}-2.jpg" alt="Image">
+
+                    if(product.Offer != "")
+                    {
+                        output += `
+                        <div class="container-fluid py-5">
+                        <div class="row px-xl-5">
+                            <div class="col-lg-5 pb-5">
+                                <div id="product-carousel" class="carousel slide" data-ride="carousel">
+                                    <div class="carousel-inner border">
+                                        <div class="carousel-item active">
+                                            <img class="w-100 h-100" src="img/${product.PID}.jpg" alt="Image">
+                                        </div>
+                                        <div class="carousel-item">
+                                            <img class="w-100 h-100" src="img/${product.PID}-1.jpg" alt="Image">
+                                        </div>
+                                        <div class="carousel-item">
+                                            <img class="w-100 h-100" src="img/${product.PID}-2.jpg" alt="Image">
+                                        </div>
+                                    </div>
+                                    <a class="carousel-control-prev" href="#product-carousel" data-slide="prev">
+                                        <i class="fa fa-2x fa-angle-left text-dark"></i>
+                                    </a>
+                                    <a class="carousel-control-next" href="#product-carousel" data-slide="next">
+                                        <i class="fa fa-2x fa-angle-right text-dark"></i>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-lg-7 pb-5">
+                                <h3 class="font-weight-semi-bold">${product.Name}</h3>
+                                <h3 class="text-muted ml-2"><del>$${product.Price}</del></h3>
+                                <h3 class="font-weight-semi-bold mb-4">Oferta: $${product.Offer}</h3>
+                                <p class="mb-4">${product.Detail}</p>
+                                <div class="d-flex align-items-center mb-4 pt-2">
+                                    <div class="input-group quantity mr-3" style="width: 130px;">
+                                        <div class="input-group-btn">
+                                            <button class="btn btn-primary btn-minus" >
+                                            <i class="fa fa-minus"></i>
+                                            </button>
+                                        </div>
+                                        <input type="text" class="form-control bg-secondary text-center" value="1">
+                                        <div class="input-group-btn">
+                                            <button class="btn btn-primary btn-plus">
+                                                <i class="fa fa-plus"></i>
+                                            </button>
+                                        </div>
+                                    </div>    
+                                    <a href="https://wa.me/+50769044732?text=Hola%20Estoy%20Interesado%20en%20este%20producto:%20${product.Name}"><i class="fa fa-whatsapp" style="font-size:48px;color:green"></i></a>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <a class="carousel-control-prev" href="#product-carousel" data-slide="prev">
-                        <i class="fa fa-2x fa-angle-left text-dark"></i>
-                    </a>
-                    <a class="carousel-control-next" href="#product-carousel" data-slide="next">
-                        <i class="fa fa-2x fa-angle-right text-dark"></i>
-                    </a>
-                </div>
-            </div>
-            <div class="col-lg-7 pb-5">
-                <h3 class="font-weight-semi-bold">${product.Name}</h3>
-                <h3 class="font-weight-semi-bold mb-4">$${product.Price}</h3>
-                <p class="mb-4">${product.Detail}</p>
-                <div class="d-flex align-items-center mb-4 pt-2">
-                    <div class="input-group quantity mr-3" style="width: 130px;">
-                        <div class="input-group-btn">
-                            <button class="btn btn-primary btn-minus" >
-                            <i class="fa fa-minus"></i>
-                            </button>
+                          `;
+                    }
+                    else {
+                        output += `
+                        <div class="container-fluid py-5">
+                        <div class="row px-xl-5">
+                            <div class="col-lg-5 pb-5">
+                                <div id="product-carousel" class="carousel slide" data-ride="carousel">
+                                    <div class="carousel-inner border">
+                                        <div class="carousel-item active">
+                                            <img class="w-100 h-100" src="img/${product.PID}.jpg" alt="Image">
+                                        </div>
+                                        <div class="carousel-item">
+                                            <img class="w-100 h-100" src="img/${product.PID}-1.jpg" alt="Image">
+                                        </div>
+                                        <div class="carousel-item">
+                                            <img class="w-100 h-100" src="img/${product.PID}-2.jpg" alt="Image">
+                                        </div>
+                                    </div>
+                                    <a class="carousel-control-prev" href="#product-carousel" data-slide="prev">
+                                        <i class="fa fa-2x fa-angle-left text-dark"></i>
+                                    </a>
+                                    <a class="carousel-control-next" href="#product-carousel" data-slide="next">
+                                        <i class="fa fa-2x fa-angle-right text-dark"></i>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-lg-7 pb-5">
+                                <h3 class="font-weight-semi-bold">${product.Name}</h3>
+                                <h3 class="font-weight-semi-bold mb-4">$${product.Price}</h3>
+                                <p class="mb-4">${product.Detail}</p>
+                                <div class="d-flex align-items-center mb-4 pt-2">
+                                    <div class="input-group quantity mr-3" style="width: 130px;">
+                                        <div class="input-group-btn">
+                                            <button class="btn btn-primary btn-minus" >
+                                            <i class="fa fa-minus"></i>
+                                            </button>
+                                        </div>
+                                        <input type="text" class="form-control bg-secondary text-center" value="1">
+                                        <div class="input-group-btn">
+                                            <button class="btn btn-primary btn-plus">
+                                                <i class="fa fa-plus"></i>
+                                            </button>
+                                        </div>
+                                    </div>    
+                                    <a href="https://wa.me/+50769044732?text=Hola%20Estoy%20Interesado%20en%20este%20producto:%20${product.Name}"><i class="fa fa-whatsapp" style="font-size:48px;color:green"></i></a>
+                                </div>
+                            </div>
                         </div>
-                        <input type="text" class="form-control bg-secondary text-center" value="1">
-                        <div class="input-group-btn">
-                            <button class="btn btn-primary btn-plus">
-                                <i class="fa fa-plus"></i>
-                            </button>
-                        </div>
-                    </div>    
-                    <a href="https://wa.me/+50769044732?text=Hola%20Estoy%20Interesado%20en%20este%20producto:%20${product.Name}"><i class="fa fa-whatsapp" style="font-size:48px;color:green"></i></a>
-                </div>
-            </div>
-        </div>
-    </div>
-          `;
-          
+                    </div>
+                        `;
+                    }
           return false;
 
                   }
--- js/shop.js
+++ js/shop.js
@@ -21,32 +21,31 @@ category = urlParams.get('category');
 
       if(producttype == null)
       {
-              output += `
-                        <div class="col-lg-4 col-md-6 col-sm-12 pb-1">
-                        <div class="card product-item border-0 mb-4">
-                            <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-                                <img class="img-fluid w-100" src="${product.PicUrl}" alt="">
-                            </div>
-                            <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
-                                <h6 class="text-truncate mb-3">${product.Name}</h6>
-                                <div class="d-flex justify-content-center">
-                                    <h6>$${product.Price}</h6>
-                                </div>
-                            </div>
-                            <div class="card-footer d-flex justify-content-between bg-light border">
-                                <a href="detail.html?productid=${product.PID}" class="btn btn-sm text-dark p-0"><i class="fas fa-eye text-primary mr-1"></i>Detalle</a>
-                                <a href="https://wa.me/+50769044732?text=Hola%20Estoy%20Interesado%20en%20este%20producto:%20${product.PID}"><i class="fa fa-whatsapp" style="font-size:24px;color:green"></i></a>
-                            </div>
+        if(product.Offer != "")
+        {
+            output += `
+            <div class="col-lg-4 col-md-6 col-sm-12 pb-1">
+                <div class="card product-item border-0 mb-4">
+                    <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
+                        <img class="img-fluid w-100" src="${product.PicUrl}" alt="">
+                    </div>
+                    <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
+                        <h6 class="text-truncate mb-3">${product.Name}</h6>
+                        <div class="d-flex justify-content-center">
+                        <h6 class="text-muted ml-2"><del>$${product.Price}</del></h6>
+                        <h6 class="font-weight-semi-bold mb-4">&nbsp;$${product.Offer}</h6>
                         </div>
                     </div>
-                      `;
-      }
-      else
-      {
-                if (product.PType != null) {
-                  if (product.PType.toLowerCase().includes(producttype.toLowerCase()) &&
-                      (product.Category.toLowerCase().includes(category.toLowerCase()))) {
-                      output += `
+                    <div class="card-footer d-flex justify-content-between bg-light border">
+                        <a href="detail.html?productid=${product.PID}" class="btn btn-sm text-dark p-0"><i class="fas fa-eye text-primary mr-1"></i>Detalle</a>
+                        <a href="https://wa.me/+50769044732?text=Hola%20Estoy%20Interesado%20en%20este%20producto:%20${product.PID}"><i class="fa fa-whatsapp" style="font-size:24px;color:green"></i></a>
+                    </div>
+                </div>
+            </div>
+          `;
+        }
+        else {
+            output += `
             <div class="col-lg-4 col-md-6 col-sm-12 pb-1">
             <div class="card product-item border-0 mb-4">
                 <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
@@ -60,11 +59,63 @@ category = urlParams.get('category');
                 </div>
                 <div class="card-footer d-flex justify-content-between bg-light border">
                     <a href="detail.html?productid=${product.PID}" class="btn btn-sm text-dark p-0"><i class="fas fa-eye text-primary mr-1"></i>Detalle</a>
-                    <a href="https://wa.me/+50769044732?text=Hola%20Estoy%20Interesado%20en%20este%20producto:%20${product.Name}"><i class="fa fa-whatsapp" style="font-size:24px;color:green"></i></a>
+                    <a href="https://wa.me/+50769044732?text=Hola%20Estoy%20Interesado%20en%20este%20producto:%20${product.PID}"><i class="fa fa-whatsapp" style="font-size:24px;color:green"></i></a>
                 </div>
             </div>
         </div>
           `;
+        }
+      }
+      else
+      {
+                if (product.PType != null) {
+                  if (product.PType.toLowerCase().includes(producttype.toLowerCase()) &&
+                      (product.Category.toLowerCase().includes(category.toLowerCase()))) {
+
+                        if(product.Offer != "")
+                        {
+                            output += `
+                            <div class="col-lg-4 col-md-6 col-sm-12 pb-1">
+                                <div class="card product-item border-0 mb-4">
+                                    <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
+                                        <img class="img-fluid w-100" src="${product.PicUrl}" alt="">
+                                    </div>
+                                    <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
+                                        <h6 class="text-truncate mb-3">${product.Name}</h6>
+                                        <div class="d-flex justify-content-center">
+                                        <h6 class="text-muted ml-2"><del>$${product.Price}</del></h6>
+                                        <h6 class="font-weight-semi-bold mb-4">&nbsp;$${product.Offer}</h6>
+                                        </div>
+                                    </div>
+                                    <div class="card-footer d-flex justify-content-between bg-light border">
+                                        <a href="detail.html?productid=${product.PID}" class="btn btn-sm text-dark p-0"><i class="fas fa-eye text-primary mr-1"></i>Detalle</a>
+                                        <a href="https://wa.me/+50769044732?text=Hola%20Estoy%20Interesado%20en%20este%20producto:%20${product.Name}"><i class="fa fa-whatsapp" style="font-size:24px;color:green"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        `;
+                        }
+                        else {
+                            output += `
+                                    <div class="col-lg-4 col-md-6 col-sm-12 pb-1">
+                                    <div class="card product-item border-0 mb-4">
+                                        <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
+                                            <img class="img-fluid w-100" src="${product.PicUrl}" alt="">
+                                        </div>
+                                        <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
+                                            <h6 class="text-truncate mb-3">${product.Name}</h6>
+                                            <div class="d-flex justify-content-center">
+                                                <h6>$${product.Price}</h6>
+                                            </div>
+                                        </div>
+                                        <div class="card-footer d-flex justify-content-between bg-light border">
+                                            <a href="detail.html?productid=${product.PID}" class="btn btn-sm text-dark p-0"><i class="fas fa-eye text-primary mr-1"></i>Detalle</a>
+                                            <a href="https://wa.me/+50769044732?text=Hola%20Estoy%20Interesado%20en%20este%20producto:%20${product.Name}"><i class="fa fa-whatsapp" style="font-size:24px;color:green"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                `;
+                        }
                   }
               }
       }
